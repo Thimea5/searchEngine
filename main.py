ac56b1036cc1797287d4ba2b69d382b4139893d0
Thimea <84723445+Thimea5@users.noreply.github.com>
2023-11-30 17:01:05 +0100

    TD4
    Ajout TD4

--- main.py
+++ main.py
@@ -1,80 +1,162 @@
-#################### TD 3 ####################
 
-#import
 import praw
+import urllib, urllib.request
 import xmltodict
-import urllib.request
-import numpy as np    
-import pandas as pd
+import pickle
+import datetime
+from Corpus import Corpus
+from Classes import Document
+from Classes import Author
 
-#Partie 1
-docs = [] #Liste qui ne contiendra que des documents
+# Fonction affichage hiérarchie dict
+def showDictStruct(d):
+    def recursivePrint(d, i):
+        for k in d:
+            if isinstance(d[k], dict):
+                print("-"*i, k)
+                recursivePrint(d[k], i+2)
+            else:
+                print("-"*i, k, ":", d[k])
+    recursivePrint(d, 1)
 
-textes_Arxiv = []
-textes_reddit = []
-
-#Connexion à l'API reddit
+# Connexion à l'API REDDIT
 reddit = praw.Reddit(client_id='k9t9Uh4CiOx2YbY1Fq1o4g', client_secret='WaaCVpa9njLgkCD1eOfwQo-OBS_sow', user_agent='td3Python')
 
-#Sujet de la recherche
-subj = reddit.subreddit('Space')
+# limite et sujet de la requête
+limit = 100
+hot_posts = reddit.subreddit('space').hot(limit=limit)#.top("all", limit=limit)#
 
-#Alimenter la liste docs avec essentiellement le champs texte avec REDDIT
-for post in subj.hot(limit=100):
-    texte = post.title
-    texte = texte.replace("\n", " ")
-    textes_reddit.append(texte)
+# Récupération du texte
+docs = []
+docs_bruts = []
+afficher_cles = False
+for i, post in enumerate(hot_posts):
+    if i%10==0: print("Reddit:", i, "/", limit)
+    if afficher_cles:  # Pour connaître les différentes variables et leur contenu
+        for k, v in post.__dict__.items():
+            pass
+            print(k, ":", v)
 
-#Connexion à l'API Arxiv
-query = "Space"
-url = 'http://export.arxiv.org/api/query?search_query=all:' + query + '&start=0&max_results=100'
-url_read = urllib.request.urlopen(url).read()
+    if post.selftext != "":  # Osef des posts sans texte
+        pass
+        #print(post.selftext)
+    docs.append(post.selftext.replace("\n", " "))
+    docs_bruts.append(("Reddit", post))
 
-#décoder le "byte stream" url_read
-data = url_read.decode()
-
-#obtenir un obj json avec xmltodict
-dico = xmltodict.parse(data)
-docs = dico['feed']['entry']
-
-
-for d in docs:
-    texte = d['title']+ ". " + d['summary']
-    texte = texte.replace("\n", " ")
-    textes_Arxiv.append(texte)
-
-# on concatène (ajout des textes reddit et des textes arxiv):
-    
-corpus = textes_reddit + textes_Arxiv
-
-#print("Corpus length: %d" % len(corpus))
-print("Longueur du corpus : " + str(len(corpus)))
+#print(docs)
 
 
-#Partie 2
+# Paramètres
+query_terms = ["clustering", "Dirichlet"]
+max_results = 50
 
-# 2.1 Créer un tableau de type DataFrame avec pandas
-data = {'ID': range(1, len(corpus) + 1),
-        'Texte': corpus,
-        'Origine': ['reddit'] * len(textes_reddit) + ['arxiv'] * len(textes_Arxiv)}
+# Requête ARXIV
+url = f'http://export.arxiv.org/api/query?search_query=all:{"+".join(query_terms)}&start=0&max_results={max_results}'
+data = urllib.request.urlopen(url)
 
-df = pd.DataFrame(data)
+# Format dict (OrderedDict)
+data = xmltodict.parse(data.read().decode('utf-8'))
 
-# 2.2 Sauvegarder le tableau sur le disque en tant que fichier CSV avec séparateur de tabulation
-csv_path = 'corpus_data.csv'
-df.to_csv(csv_path, sep='\t', index=False)
+#showDictStruct(data)
 
-# 2.3 Charger le tableau directement en mémoire à partir du fichier CSV
-loaded_df = pd.read_csv('corpus_data.csv', sep='\t')
+# Ajout résumés à la liste
+for i, entry in enumerate(data["feed"]["entry"]):
+    if i%10==0: print("ArXiv:", i, "/", limit)
+    docs.append(entry["summary"].replace("\n", ""))
+    docs_bruts.append(("ArXiv", entry))
+    #showDictStruct(entry)
+
+# =============== 1.3 : Exploitation ===============
+print(f"# docs avec doublons : {len(docs)}")
+docs = list(set(docs))
+print(f"# docs sans doublons : {len(docs)}")
+
+for i, doc in enumerate(docs):
+    print(f"Document {i}\t# caractères : {len(doc)}\t# mots : {len(doc.split(' '))}\t# phrases : {len(doc.split('.'))}")
+    if len(doc)<100:
+        docs.remove(doc)
+
+longueChaineDeCaracteres = " ".join(docs)
+
+# =============== PARTIE 2 =============
+# =============== 2.3 : MANIPS ===============
+collection = []
+for nature, doc in docs_bruts:
+    if nature == "ArXiv":  # Les fichiers de ArXiv ou de Reddit sont pas formatés de la même manière à ce stade.
+        #showDictStruct(doc)
+
+        titre = doc["title"].replace('\n', '')  # On enlève les retours à la ligne
+        try:
+            authors = ", ".join([a["name"] for a in doc["author"]])  # On fait une liste d'auteurs, séparés par une virgule
+        except:
+            authors = doc["author"]["name"]  # Si l'auteur est seul, pas besoin de liste
+        summary = doc["summary"].replace("\n", "")  # On enlève les retours à la ligne
+        date = datetime.datetime.strptime(doc["published"], "%Y-%m-%dT%H:%M:%SZ").strftime("%Y/%m/%d")  # Formatage de la date en année/mois/jour avec librairie datetime
+
+        doc_classe = Document(titre, authors, date, doc["id"], summary)  # Création du Document
+        collection.append(doc_classe)  # Ajout du Document à la liste.
+
+    elif nature == "Reddit":
+        #print("".join([f"{k}: {v}\n" for k, v in doc.__dict__.items()]))
+        titre = doc.title.replace("\n", '')
+        auteur = str(doc.author)
+        date = datetime.datetime.fromtimestamp(doc.created).strftime("%Y/%m/%d")
+        url = "https://www.reddit.com/"+doc.permalink
+        texte = doc.selftext.replace("\n", "")
+
+        doc_classe = Document(titre, auteur, date, url, texte)
+
+        collection.append(doc_classe)
+
+# Création de l'index de documents
+id2doc = {}
+for i, doc in enumerate(collection):
+    id2doc[i] = doc.titre
+
+# =============== 2.6 : DICT AUTEURS ===============
+authors = {}
+aut2id = {}
+num_auteurs_vus = 0
+
+# Création de la liste+index des Auteurs
+for doc in collection:
+    if doc.auteur not in aut2id:
+        num_auteurs_vus += 1
+        authors[num_auteurs_vus] = Author(doc.auteur)
+        aut2id[doc.auteur] = num_auteurs_vus
+
+    authors[aut2id[doc.auteur]].add(doc.texte)
 
 
-#Partie 3
-#1
-#print("Corpus length: %d" % len(corpus))
-print("Longueur du corpus : " + str(len(corpus)))
+# =============== 2.7, 2.8 : CORPUS ===============
+corpus = Corpus("Mon corpus")
+
+# Construction du corpus à partir des documents
+for doc in collection:
+    corpus.add(doc)
+#corpus.show(tri="abc")
+#print(repr(corpus))
 
 
-for doc in corpus:
-    # nombre de phrases
-    print("Nombre de phrases : " + str(len(doc.split("."))))
-    print("Nombre de mots : " + str(len(doc.split(" "))))
+# =============== 2.9 : SAUVEGARDE ===============
+# Ouverture d'un fichier, puis écriture avec pickle
+with open("corpus.pkl", "wb") as f:
+    pickle.dump(corpus, f)
+
+# Supression de la variable "corpus"
+del corpus
+
+# Ouverture du fichier, puis lecture avec pickle
+with open("corpus.pkl", "rb") as f:
+    corpus = pickle.load(f)
+
+# La variable est réapparue
+print(corpus)
+
+
+
+
+
+
+
+
